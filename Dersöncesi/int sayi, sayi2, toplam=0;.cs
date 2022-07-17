58387555193aaeaebe71f7d656635117a1a2f240
F4250-Akif KA <karaozmehmetakif@gmail.com>
2022-07-17 21:11:36 +0300

    list dosyası

---
+++ Dersöncesi/int sayi, sayi2, toplam=0;.cs
@@ -0,0 +1,8 @@
+int sayi, sayi2, toplam=0;
+Consol.Write("Birinci Sayı:")
+sayi=Convert.ToInt16(Consol.Read());
+Consol.Write("İkinci Sayı:")
+sayi2=Convert.ToInt16(Consol.Read());
+toplam=sayi+sayi2;
+Consol.Write("Toplam:",+,toplam);
+Consol.ReadKey();
